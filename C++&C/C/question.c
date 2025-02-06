e4fb252aa8ee2406fb93b0108faeab017911254e
Sarthak-kumar-6843 <sarthakkumar.k6843@gmail.com>
2025-02-07 00:09:21 +0530

    update

---
+++ C++&C/C/question.c
@@ -0,0 +1,10 @@
+//area of square
+#include <stdio.h>
+int main(){
+    int side;
+    printf("enter side : ");
+    scanf("%d", &side);
+
+    printf(" area is : %d" , side *side );
+    return 0;
+}
---
+++ C++&C/C/question2.c
@@ -0,0 +1,10 @@
+//area of square
+#include <stdio.h>
+int main(){
+float radius;
+printf("enter radius : " );
+scanf("%f",&radius);
+
+printf(" area of circle : %f", 3.14*radius*radius);
+   return 0;
+}
